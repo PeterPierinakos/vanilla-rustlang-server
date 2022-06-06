98f28a66215bd75d5c810cdf5c838f4e6d03f3dd
PeterPierinakos <petergit@proton.me>
2022-06-06 14:42:20 +0000

    Async support, setup script and stock HTML files

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,10 +1,12 @@
 /*
+WARNING: LOGGING IS DISABLED WHEN USING ASYNC. ASYNC IS USUALLY NOT NECESSARY FOR A STATIC WEB SERVER AS IT BARELY INCREASES PERFORMANCE.
 
 ABSOLUTE_STATIC_CONTENT_PATH: The absolute path for your HTML/CSS files.
 ABSOLUTE_LOGS_PATH: The absolute path for the location to store the log files at runtime (you may keep it blank if you have set "SAVE_LOGS" to "false".)
 PORT: The port used by the server.
 ADDR: The address used by the server. IPv4 only. Use "127.0.0.1" for localhost.
 SAVE_LOGS: Change to "false" if you don't want logs of the requests to be saved at runtime.
+ASYNC: Whether you want to handle connections with several workers instead of a single one.
 
 Tree example for ABSOLUTE_STATIC_CONTENT_PATH:
 ABSOLUTE_STATIC_CONTENT_PATH/
@@ -19,3 +21,4 @@ pub const ABSOLUTE_LOGS_PATH: &str = "/var/www/logs";
 pub const ADDR: &str = "127.0.0.1";
 pub const PORT: u32 = 80;
 pub const SAVE_LOGS: bool = true;
+pub const ASYNC: bool = true;
--- src/core/server.rs
+++ src/core/server.rs
@@ -2,16 +2,54 @@ use crate::configuration::*;
 use crate::constants::*;
 use crate::core::time::generate_unixtime;
 use crate::structs::uri::URI;
+use crate::util::response::*;
 use std::io::Write;
 use std::str;
+use std::thread;
 use std::{fs, fs::File, fs::OpenOptions};
 use std::{
     io::Read,
     net::{TcpListener, TcpStream},
 };
-use crate::util::response::*;
 
-fn handle_connection(logfile: &Option<File>, mut stream: TcpStream) {
+fn async_handle_connection(mut stream: TcpStream) {
+    let mut buf = [0; 1024];
+
+    stream.read(&mut buf).unwrap();
+
+    if !buf.starts_with(GET) {
+        let response = response_400();
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+        return;
+    }
+
+    let mut uri = URI::new();
+
+    uri.find(&str::from_utf8(&buf).unwrap().to_string());
+
+    if uri.get() == &None {
+        let response = response_400();
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+        return;
+    };
+
+    let requested_content = fs::read_to_string(format!(
+        "{}/html/{}",
+        ABSOLUTE_STATIC_CONTENT_PATH,
+        uri.get().clone().unwrap()
+    ));
+    let response = match requested_content {
+        Ok(file) => response_success(file),
+        Err(_err) => response_404(),
+    };
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
+fn handle_sync_connection(logfile: &Option<File>, mut stream: TcpStream) {
     let mut buf = [0; 1024];
 
     stream.read(&mut buf).unwrap();
@@ -55,7 +93,7 @@ fn handle_connection(logfile: &Option<File>, mut stream: TcpStream) {
         uri.get().clone().unwrap()
     ));
     let response = match requested_content {
-        Ok(file) => response_success(file), 
+        Ok(file) => response_success(file),
         Err(_err) => response_404(),
     };
 
@@ -81,6 +119,16 @@ pub fn start_server(unixtime: u64) {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(&logfile, stream);
+        if ASYNC {
+            handle_thread(stream);
+        } else {
+            handle_sync_connection(&logfile, stream)
+        }
     }
+}
+
+pub fn handle_thread(stream: TcpStream) {
+    thread::spawn(|| {
+        async_handle_connection(stream);
+    });
 }
