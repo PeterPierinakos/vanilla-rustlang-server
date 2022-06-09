f38092c7a174326ade1e557afa47d2f62ac978b1
PeterPierinakos <petergit@proton.me>
2022-06-09 12:22:27 +0000

    Add production note

--- src/configuration.rs
+++ src/configuration.rs
@@ -4,7 +4,10 @@ Documentation in manual.html inside media/
 
 pub const ABSOLUTE_STATIC_CONTENT_PATH: &str = "/var/www/static";
 pub const ABSOLUTE_LOGS_PATH: &str = "/var/www/logs";
-pub const ADDR: &str = "127.0.0.1";
+
+/* Production note: "ADDR" should be "0.0.0.0" when running inside a Docker container. */
+pub const ADDR: &str = "0.0.0.0";
+
 pub const PORT: u32 = 80;
 pub const SAVE_LOGS: bool = true;
 pub const MULTITHREADING: bool = true;
