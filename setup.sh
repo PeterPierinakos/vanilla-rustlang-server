87de703b055567f659d2fa6b87aa35022855955d
PeterPierinakos <petergit@proton.me>
2022-06-09 12:35:28 +0000

    Update version number to prepare for first release and improve setup script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-echo "Reminder: make sure you have set up your configuration.rs file in src/ before running this script (press any key to continue)"
-read
-
-echo "Please enter the directory for the static files you have set (recommended: /var/www/static) (no leading slash)"
-read STATIC
-echo "Please enter the directory for the logs you have set (recommended: /var/www/logs) (no leading slash)"
-read LOGS
+STATIC=/var/www/static
+LOGS=/var/www/logs
 
 echo "rm -rf $STATIC/"
 rm -rf $STATIC/
