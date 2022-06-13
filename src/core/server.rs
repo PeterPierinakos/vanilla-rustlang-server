376e050ed4f86c4cd96fb5d17e82633878d8fb31
PeterPierinakos <petergit@proton.me>
2022-06-13 13:21:42 +0000

    Fix server crashes from trying to read invalid req buffer

--- src/core/server.rs
+++ src/core/server.rs
@@ -3,26 +3,48 @@ use crate::core::time::generate_unixtime;
 use crate::enums::methods::HttpRequestMethod;
 use crate::structs::cors::Cors;
 use crate::structs::uri::URI;
+use crate::util::license::print_license_info;
 use crate::util::response::*;
+use crate::util::socket::handle_stream;
 use std::collections::HashSet;
 use std::io::Write;
-use std::str;
+use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::{fs, fs::File, fs::OpenOptions};
-use std::{
-    io::Read,
-    net::{TcpListener, TcpStream},
-};
-use crate::util::license::print_license_info;
+use crate::util::socket::parse_utf8;
 
 fn multithread_handle_connection(mut stream: TcpStream) {
-    let mut buf = [0; 1024];
+    let buf = handle_stream(&mut stream);
 
-    stream.read(&mut buf).unwrap();
+    /* Handle connections that send no data. */
+    /* === */
+
+
+    if buf.is_none() {
+        let response = response_500();
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+        return;
+    }
+
+    let buf = buf.unwrap();
+
+    let buf_utf8 = parse_utf8(&buf);
+
+    if buf_utf8.is_none() { 
+        let response = response_500();
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+        return;
+    }
+
+    let buf_utf8 = buf_utf8.unwrap();
+
+    /* === */
 
     let cors = Cors::new(HashSet::from([HttpRequestMethod::GET]));
 
-    if !cors.method_is_allowed(str::from_utf8(&buf).unwrap().to_string()) {
+    if !cors.method_is_allowed(&buf_utf8) {
         let response = response_400();
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
@@ -31,7 +53,7 @@ fn multithread_handle_connection(mut stream: TcpStream) {
 
     let mut uri = URI::new();
 
-    uri.find(&str::from_utf8(&buf).unwrap().to_string());
+    uri.find(&buf_utf8);
 
     if uri.get() == &None {
         let response = response_400();
@@ -54,9 +76,32 @@ fn multithread_handle_connection(mut stream: TcpStream) {
 }
 
 fn handle_sync_connection(logfile: &Option<File>, mut stream: TcpStream) {
-    let mut buf = [0; 1024];
+    let buf = handle_stream(&mut stream);
 
-    stream.read(&mut buf).unwrap();
+    /* Handle connections that send no data. */
+    /* === */
+
+    if buf.is_none() {
+        let response = response_500();
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+        return;
+    }
+
+    let buf = buf.unwrap();
+
+    let buf_utf8 = parse_utf8(&buf);
+
+    if buf_utf8.is_none() { 
+        let response = response_500();
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+        return;
+    }
+
+    let buf_utf8 = buf_utf8.unwrap();
+
+    /* === */
 
     if !logfile.is_none() {
         logfile
@@ -67,7 +112,7 @@ fn handle_sync_connection(logfile: &Option<File>, mut stream: TcpStream) {
                     "REQUEST AT {}\nREQUEST IP ADDRESS: {}\nINFO: {}\n===========\n",
                     generate_unixtime(),
                     stream.local_addr().unwrap(),
-                    str::from_utf8(&buf).unwrap(),
+                    buf_utf8
                 )
                 .as_bytes(),
             )
@@ -76,7 +121,7 @@ fn handle_sync_connection(logfile: &Option<File>, mut stream: TcpStream) {
 
     let cors = Cors::new(HashSet::from([HttpRequestMethod::GET]));
 
-    if !cors.method_is_allowed(str::from_utf8(&buf).unwrap().to_string()) {
+    if !cors.method_is_allowed(&buf_utf8) {
         let response = response_400();
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
@@ -85,7 +130,7 @@ fn handle_sync_connection(logfile: &Option<File>, mut stream: TcpStream) {
 
     let mut uri = URI::new();
 
-    uri.find(&str::from_utf8(&buf).unwrap().to_string());
+    uri.find(&buf_utf8);
 
     if uri.get() == &None {
         let response = response_400();
