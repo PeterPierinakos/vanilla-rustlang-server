06aed61c02479fbf61eb367bf59d41fff0deb421
PeterPierinakos <peterpierinakos-business@protonmail.com>
2022-06-13 08:43:47 +0000

    Possible fix for path traversal exploit

--- src/structs/uri.rs
+++ src/structs/uri.rs
@@ -1,14 +1,20 @@
 pub struct URI {
     uri: Option<String>,
+    found_error: bool,
 }
 
 impl URI {
     pub fn new() -> URI {
-        URI { uri: None }
+        URI {
+            uri: None,
+            found_error: false,
+        }
     }
 
     pub fn find(&mut self, headers_buffer: &String) {
         let mut uri = String::new();
+
+        let mut dot_c = 0;
 
         for (i, c) in headers_buffer.chars().enumerate() {
             // After the "GET" in the INFO header.
@@ -16,11 +22,20 @@ impl URI {
                 break;
             }
             if i > 4 {
+                if c == '.' {
+                    dot_c += 1;
+                }
                 uri.push(c);
             }
         }
 
-        if uri.is_empty() { 
+        if dot_c > 1 {
+            self.found_error = true;
+            self.uri = None;
+            return;
+        }
+
+        if uri.is_empty() {
             self.uri = Some("index.html".to_string());
             return;
         }
