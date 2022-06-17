04462b5952ba075a4f5b3f76ec42b276359a1521
PeterPierinakos <peterpierinakos-business@protonmail.com>
2022-06-17 18:28:54 +0000

    Minor improvement configuration's organization for context

--- src/configuration.rs
+++ src/configuration.rs
@@ -9,14 +9,15 @@ pub const ABSOLUTE_LOGS_PATH: &str = "/var/www/logs";
 
 /* Production note: "ADDR" should be "0.0.0.0" when running inside a Docker container. */
 pub const ADDR: &str = "0.0.0.0";
-
 pub const PORT: u32 = 80;
-pub const SAVE_LOGS: bool = true;
 
 /* Production note: Multithreaded mode currently doesn't support logs. */
 pub const MULTITHREADING: bool = false;
+pub const SAVE_LOGS: bool = true;
 
 pub const HTTP_PROTOCOL_VERSION: HttpProtocolVersion = HttpProtocolVersion::OneDotOne;
+
+/* Security configuration below */
 
 /* Production note: You should allow all origins if you want everyone to access your page. If you set "ALLOW_ALL_ORIGINS" to true, you may keep "ALLOWED_ORIGINS" empty. */
 pub const ALLOW_ALL_ORIGINS: bool = true;
@@ -24,5 +25,4 @@ pub const ALLOWED_ORIGINS: [&str; 0] = [];
 
 /* Production note: turning this off is not recommended and only for debugging purposes */
 pub const SECURITY_HEADERS: bool = true;
-
 pub const ALLOW_IFRAMES: bool = false;
