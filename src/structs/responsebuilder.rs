981e9f4a1e3b9db6af43876984a21f442eba6d47
PeterPierinakos <petergit@proton.me>
2022-06-20 15:50:56 +0000

    Remove debugging info

--- src/structs/responsebuilder.rs
+++ src/structs/responsebuilder.rs
@@ -98,8 +98,6 @@ impl<'a> ResponseBuilder<'a> {
         response.push_str("\r\n\r\n");
         response.push_str(self.body.unwrap());
 
-        println!("{}", response);
-
         response
     }
 }
